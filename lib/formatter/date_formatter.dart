77929fa3cf42d1a0debef476552be8f1a08b2996
abhaym <3100abma@nibe.se>
2021-06-16 16:04:51 +0530

    fix: typo in date formatter

--- lib/formatter/date_formatter.dart
+++ lib/formatter/date_formatter.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 String getTimestampToDate(
     BuildContext context, double timestamp, String dateFormat) {
   var date = DateTime.fromMillisecondsSinceEpoch(timestamp.toInt());
-  var formattor =
+  var formatter =
       DateFormat(dateFormat, Localizations.localeOf(context).countryCode);
-  return formattor.format(date);
+  return formatter.format(date);
 }
